cec88257e597ce27e4c01ee73a9a1529f831df6e
Nicolas Boffo <106088482+NicolasChief@users.noreply.github.com>
2025-10-26 16:20:58 -0300

    Add game logic for EscolhaPorta

--- src/model/minigames/EscolhaPorta.java
+++ src/model/minigames/EscolhaPorta.java
@@ -4,13 +4,22 @@ import model.EstruturaDados;
 import model.Jogo;
 import model.Dificuldade;
 
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
 public class EscolhaPorta extends Jogo {
 
+	private No inicio;
+    private No fim;
+    private int tamanhoCores;
+    private int tamanhoPorta;
+    private int[] portas;
+	
 	public EscolhaPorta(Dificuldade dificuldade) {
 		super("EscolhaPorta", dificuldade, EstruturaDados.LISTA,
 		"Descricao do Jogo",
 		"Instrucoes do Jogo");
-		// Adicione aqui outros parametros iniciais necessarios
 	}
 
 	@Override
@@ -27,4 +36,187 @@ public class EscolhaPorta extends Jogo {
 		return this.estrutura;
 	}
 
+	// Fim Parte Gabs
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Dificuldade getDificuldade() {
+		return dificuldade;
+	}
+
+	public EstruturaDados getEstrutura() {
+		return estrutura;
+	}
+
+	public int getPontuacao() {
+		return pontuacao;
+	}
+
+    public void inserir(int cor) { // Insere cor á lista
+    	
+        No novo = new No(cor);
+        if (inicio == null) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            fim.setProximo(novo);
+            fim = novo;
+        }
+        tamanhoCores++;
+        
+    }
+    
+    public No getInicio() { // Pega a primeira cor da fila
+        return inicio;
+    }
+
+    public int gerarCor() { // Gera a cor
+    	
+        Random r = new Random();
+        int cor;
+//      return new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)); // Para caso utilize o import
+        cor = r.nextInt(50); // Os números são as cores
+        return cor;
+        
+    }
+
+    public void imprimir() { // Era teste pra fila
+        No atual = inicio;
+        while (atual != null) {
+            int c = atual.getColor();
+            System.out.println(c);
+            atual = atual.getProximo();
+        }
+    }
+
+    public int getTamanho() { // Tamanho da fila
+        return tamanhoCores;
+    }
+    
+    public void cliqueCerto() { // Pontuação padrão
+    	
+    	switch (dificuldade) {
+        
+    	case FACIL -> pontuacao += 10;
+        
+    	case MEDIO -> pontuacao += 15;
+        
+    	case DIFICIL -> pontuacao += 20;
+    	
+     }
+    	proximaFila();
+    	gerarPortasNovas();
+    }
+    
+    public void cliqueErrado() { // Caso jogador erre a porta
+		switch (dificuldade) {
+			case FACIL: pontuacao -= 5;
+						proximaFila();
+						gerarPortasNovas();
+			break;
+		
+			case MEDIO: pontuacao -= 10;
+						proximaFila();
+						gerarPortasNovas();
+			break;
+		
+			case DIFICIL: pontuacao -= 15;
+						  proximaFila();
+						  gerarPortasNovas();
+			break;
+		}
+	}
+    
+    public void modificadorDificuldade() { // Este método cria modificadores de fase baseados na dificuldade
+		
+		switch (dificuldade) {
+		
+			case FACIL: tamanhoPorta = 3;
+						tamanhoCores = 20;
+			break;
+			
+			case MEDIO: tamanhoPorta = 4;
+						tamanhoCores = 30;
+			break;
+			
+			case DIFICIL: tamanhoPorta = 5;
+						  tamanhoCores = 40;
+			break;
+			
+		}
+	
+	}
+    
+    public void gerarPortasNovas() { // Este método gera novas portas em caso de acerto
+		
+		Random r = new Random();
+		int cor;
+		portas = new int[tamanhoPorta];
+		
+		for(int i = 0; i < tamanhoPorta; i ++) {
+			
+			cor = r.nextInt(50);
+			portas[i] = cor;
+			
+		}
+			
+		if (inicio != null)
+			
+	        portas[r.nextInt(tamanhoPorta)] = inicio.getColor();
+		
+	}
+    
+    public boolean verificarVazio() { // Verifica se a lista está vazia
+		if (inicio == fim) {
+			pontuacaoFinal();
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean verificarCheio() { // Verifica se a lista está cheia
+		if (fim != null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public void proximaFila() { // Próxima cor da lista
+	    if (inicio != null) {
+	        inicio = inicio.getProximo(); 
+	        tamanhoCores--;
+	    }
+	}
+	
+	public void pontuacaoFinal() { // Score total
+		
+		switch (dificuldade) {
+		
+			case FACIL: pontuacao *= 1;
+			break;
+		
+			case MEDIO: pontuacao *= 1.25;
+			break;
+		
+			case DIFICIL: pontuacao *= 1.5;
+			break;
+			
+		}
+		
+	}
+	
+	public void clique(int portaClicada, MouseEvent e) { // Clique de porta padrão
+		
+		int corClicada = portas[portaClicada];
+		
+		if (inicio != null && corClicada == (inicio.getColor())) {
+			cliqueCerto();
+		} else {
+			cliqueErrado();
+		}
+	}
+	
 }
